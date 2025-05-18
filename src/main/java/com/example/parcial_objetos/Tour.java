ff3eee95910c4c6fa104ce38bca6197846d7dbf0
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2025-05-18 16:50:07 -0300

    Add: Tour.java and TourTest.java

---
+++ src/main/java/com/example/parcial_objetos/Tour.java
@@ -0,0 +1,58 @@
+package com.example.parcial_objetos;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Tour {
+
+	private final LocalDate fechaSalida;
+	private final int cantPersonasRequeridas;
+	private final Lugar[] listaLugares;
+	private final int montoAPagarPorPersona;
+	private final ArrayList<Persona> personas;
+
+	public Tour(LocalDate fechaSalida, int cantPersonasRequeridas, Lugar[] listaLugares, int montoAPagarPorPersona,
+			ArrayList<Persona> personas) {
+		this.fechaSalida = fechaSalida;
+		this.cantPersonasRequeridas = cantPersonasRequeridas;
+		this.listaLugares = listaLugares;
+		this.montoAPagarPorPersona = montoAPagarPorPersona;
+		this.personas = personas;
+	}
+
+	public void agregarPersona(Persona persona) {
+		this.validarPersonaAdecuada(persona);
+		this.incorporarPersona(persona);
+	}
+
+	private void incorporarPersona(Persona persona) {
+		this.validarTourNoConfirmado();
+		personas.add(persona);
+	}
+
+	private void validarPersonaAdecuada(Persona persona) {
+		assert persona.puedePagarElTour(montoAPagarPorPersona) : "La persona no posee el presupuesto suficiente";
+		assert persona.lugaresAdecuados(listaLugares) : "No todos los lugares son adecuados para la persona";
+	}
+
+	private void validarTourNoConfirmado() {
+		assert !this.tourConfirmado() : "El tour ya se encuentra confirmado, no se puede incorporar más gente";
+	}
+
+	public boolean tourConfirmado() {
+		return personas.size() == cantPersonasRequeridas;
+	}
+
+	public void desvincularPersona(Persona persona) {
+		personas.remove(persona);
+	}
+
+	public boolean saleEsteAnio() {
+		return fechaSalida.getYear() == LocalDate.now().getYear();
+	}
+
+	public int montoTour() {
+		return montoAPagarPorPersona * personas.size();
+	}
+
+}
---
+++ src/test/java/com/example/parcial_objetos/TourTest.java
@@ -0,0 +1,68 @@
+package com.example.parcial_objetos;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class TourTest {
+
+	private final PreferenciaVacaciones preferencia = new CombinacionCriteriosLugares(
+			new PreferenciaVacaciones[] { new LugaresTranquilos(), new LugaresRaros() });
+	private final Persona persona1 = new Persona(preferencia, 1000000);
+	private final Persona persona2 = new Persona(preferencia, 1000000);
+	private final Ciudad ciudad = new Ciudad("Nombre letras par", 200000,
+			new String[] { "Fuente", "Plaza", "Obelisco", "Rosedal" }, 30);
+	private final Pueblo pueblo = new Pueblo("Nombre par", 100, 1750, "La Pampa");
+
+	@Test
+	public void laPersonaNoPoseeSuficientePresupuesto() {
+		final Tour tour = new Tour(LocalDate.of(2023, 1, 7), 3, new Lugar[] { ciudad, pueblo }, 800000,
+				new ArrayList<Persona>(Arrays.asList(persona1, persona2)));
+		final Persona persona = new Persona(preferencia, 700000);
+		AssertionError error = assertThrows(AssertionError.class, () -> tour.agregarPersona(persona));
+		assertEquals("La persona no posee el presupuesto suficiente", error.getMessage());
+	}
+
+	@Test
+	public void laPersonaNoTienePreferenciaPorTodosLosLugares() {
+		final Tour tour = new Tour(LocalDate.of(2023, 1, 7), 3, new Lugar[] { ciudad, pueblo }, 800000,
+				new ArrayList<Persona>(Arrays.asList(persona1, persona2)));
+		final Persona persona = new Persona(new LugaresTranquilos(), 1000000);
+		AssertionError error = assertThrows(AssertionError.class, () -> tour.agregarPersona(persona));
+		assertEquals("No todos los lugares son adecuados para la persona", error.getMessage());
+	}
+
+	@Test
+	public void laPersonaPuedeSerAgregadaAlTour() {
+		final Tour tour = new Tour(LocalDate.of(2023, 1, 7), 3, new Lugar[] { ciudad, pueblo }, 800000,
+				new ArrayList<Persona>(Arrays.asList(persona1, persona2)));
+		final Persona persona = new Persona(preferencia, 1000000);
+		assertDoesNotThrow(() -> tour.agregarPersona(persona));
+		assertTrue(tour.tourConfirmado());
+	}
+
+	@Test
+	public void laPersonaNoPuedeSerAgregadaAUnTourConfirmado() {
+		final Tour tour = new Tour(LocalDate.of(2023, 1, 7), 2, new Lugar[] { ciudad, pueblo }, 800000,
+				new ArrayList<Persona>(Arrays.asList(persona1, persona2)));
+		final Persona persona = new Persona(preferencia, 1000000);
+		AssertionError error = assertThrows(AssertionError.class, () -> tour.agregarPersona(persona));
+		assertEquals("El tour ya se encuentra confirmado, no se puede incorporar más gente", error.getMessage());
+	}
+
+	@Test
+	public void unaPersonaPuedeSerDesvinculadaDelTour() {
+		final Persona persona = new Persona(preferencia, 1000000);
+		final Tour tour = new Tour(LocalDate.of(2023, 1, 7), 3, new Lugar[] { ciudad, pueblo }, 800000,
+				new ArrayList<Persona>(Arrays.asList(persona1, persona2, persona)));
+		assertDoesNotThrow(() -> tour.desvincularPersona(persona));
+		assertTrue(!tour.tourConfirmado());
+	}
+}
